d198779adc4977ebf42dc701a3b280b007190e98
Indra Gunanda <indra@imceria.com>
2020-06-30 16:54:12 +0700

    staging

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // Nuxt modules
+  modules: [
+    'nuxt7',
+    '@nuxtjs/pwa'
+  ],
+
+  // PWA manifest
+  // https://github.com/nuxt-community/pwa-module
+  manifest: {
+    name: 'BBLR Monitoring',
+    description: 'Aplikasi Untuk Membantu Monitoring BBLR'
+  },
+
+  // Framework7 Config
+  framework7: {
+    // ...
+  },
+
+  // Build configuration
+  build: {
+    // Extract CSS in a separated file
+    extractCSS: true,
+
+    // You can extend webpack config here
+    extend (config) {
+      // ...
+    }
+  },
+
+  // Additional CSS configuration
+  css: [
+    'assets/app.css'
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <f7-page login-screen>
+    <f7-login-screen-title>LOGO</f7-login-screen-title>
+    <f7-list form>
+      <f7-list-input
+        label="Username"
+        type="text"
+        placeholder="Your username"
+        :value="username"
+        @input="username = $event.target.value"
+      ></f7-list-input>
+      <f7-list-input
+        label="Password"
+        type="password"
+        placeholder="Your password"
+        :value="password"
+        @input="password = $event.target.value"
+      ></f7-list-input>
+    </f7-list>
+    <f7-list>
+      <f7-list-button @click="signIn">Sign In</f7-list-button>
+
+    </f7-list>
+  </f7-page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      signIn() {
+        const self = this;
+        const app = self.$f7;
+
+        app.dialog.alert(`Username: ${self.username}<br>Password: ${self.password}`, () => {
+          this.$f7router.navigate('/admin', {
+            props: {
+              foo: 'bar',
+              bar: true,
+            }
+          })
+        });
+      },
+    },
+  };
+</script>
